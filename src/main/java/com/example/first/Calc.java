ffb70872625d2697302e09f8703c2ee3728f9844
shawh <shawharishankr8@gmail.com>
2023-02-19 21:57:10 +0530

    sub method added

--- src/main/java/com/example/first/Calc.java
+++ src/main/java/com/example/first/Calc.java
@@ -4,9 +4,14 @@ public class Calc {
     public static void main(String[] args) {
         int a=add(1,2);
         System.out.println(a);
+        System.out.println(sub(10,5));
     }
     public static int add(int a , int b)
     {
         return a+b;
+    }
+    public static int sub(int a , int b)
+    {
+        return a-b;
     }
 }
