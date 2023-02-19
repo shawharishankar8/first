c09578bc82638102e86b8243a60b8e32ca15b8bb
shawharishankar8 <125815451+shawharishankar8@users.noreply.github.com>
2023-02-19 21:53:25 +0530

    Add files via upload

---
+++ src/main/java/com/example/first/Calc.java
@@ -0,0 +1,12 @@
+package com.example.first;
+
+public class Calc {
+    public static void main(String[] args) {
+        int a=add(1,2);
+        System.out.println(a);
+    }
+    public static int add(int a , int b)
+    {
+        return a+b;
+    }
+}
